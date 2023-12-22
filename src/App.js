92ed7f3f95405dce5580e58c6edab3152ecc854e
Sophia S. <sophia0791@icloud.com>
2023-12-22 12:17:27 +0100

    add new article function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
 import "./styles.css";
 
+function NewArticle() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Snakes</h2>
+      <label htmlFor="snake-input">
+        guckste ma welche schlange dich interessiert
+      </label>
+      <input id="snake-input"></input>
+      <a
+        className="article__link"
+        href="https://de.wikipedia.org/wiki/Schlangen"
+      >
+        Visit the wikipedia website for snakes
+      </a>
+    </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <NewArticle />;
 }
